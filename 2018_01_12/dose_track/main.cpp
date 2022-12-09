e29cd657955fae3a8d5c445efb5cc42f7c9a9c88
Mattias Erlingsson <mattias.erlingson@gmail.com>
2022-12-09 13:03:22 +0100

    Add 2018_01_12 solution

---
+++ 2018_01_12/dose_track/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+int main() {
+    std::cout << "Welcome to dose track!" << std::endl;
+    std::cout << std::endl;
+
+    int dosage;
+    std::cout << "What is the total amount of medicine (in gram)? ";
+    std::cin >> dosage;
+
+    int hours;
+    std::cout << "Over how many hours do you want to take the medicine? ";
+    std::cin >> hours;
+
+    std::cout << "The dosage is: ";
+
+    int remainder{dosage % hours};
+    int even_dosage{(dosage - remainder) / hours};
+
+    std::vector<int> result(hours);
+
+    /* Fill vector with correct dosages */
+    std::generate(result.begin(), result.end(), [=]() mutable {
+        if (remainder-- > 0) {
+            return even_dosage + 1;
+            
+        } else {
+            return even_dosage;
+        }
+    });
+    
+    /* Print dosages to standard output */
+    std::copy(result.begin(), result.end(), 
+        std::ostream_iterator<int>(std::cout, " "));
+    
+    std::cout << std::endl;
+
+
+    return 0;
+}
